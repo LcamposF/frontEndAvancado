10f3d530ea5ff9aca0c7e53ef6840f6fd9f5e6ad
LcamposF <luacampod@gmail.com>
2025-05-01 14:26:30 -0300

    exercicio aula 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Adicao from './components/Adicao'
+import Subtracao from './components/Subtracao'
+import Multiplicacao from './components/Multiplicacao'
+import Divisao from './components/Divisao'
+import PrecisoEstudar from './components/PrecisoEstudar'
+
+const App = () => {
+  return (
+    <div>
+    
+      <PrecisoEstudar />
+    </div>
+    
+  )
+}
+
+export default App
